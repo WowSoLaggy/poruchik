7f725006e536d4aa3ab75af13bb3511cc4fa9e53
WowSoLaggy <dohxehapo@gmail.com>
2025-08-21 22:27:10 +0300

    Test action

--- src/api.js
+++ src/api.js
@@ -2,17 +2,35 @@ const { get_players } = require('./utils/players.js');
 const { destroy } = require('./utils/ydb.js');
 
 
+async function get_action(event) {
+  console.log('func call \'get_action\'');
+  if (!event) {
+    console.error('No \'event\' provided');
+    throw new Error('No \'event\' provided');
+  }
+  console.log('\'event\' provided');
+  if (!event.params) {
+    console.error('No \'params\' provided');
+    throw new Error('No \'params\' provided');
+  }
+  console.log('\'params\' provided');
+  if (!event.params.proxy) {
+    console.error('No \'proxy\' provided');
+    throw new Error('No \'proxy\' provided');
+  }
+  console.log('\'proxy\' provided');
+
+  console.log('action: \'' + event.params.proxy + '\'');
+  return event.params.proxy;
+}
+
 async function on_request(event) {
   
   console.log('LOG EVENT')
-  console.log(event);
+  console.log(JSON.stringify(event));
   console.log('LOG EVENT END')
-  console.log('LOG PARAMS')
-  console.log(event.params);
-  console.log('LOG PARAMS END')
-  console.log('LOG PROXY PATH')
-  console.log(event.params.proxy);
-  console.log('LOG PROXY PATH END')
+
+  const action = await get_action(event);
 
   try {
     const players_list = await get_players();
